8425acae55725d7926866b2a6b14b1396d6bb2c0
Korolev Kirill <fenix662@yandex.ru>
2018-09-11 07:25:49 +0600

    7step

--- bot.py
+++ bot.py
@@ -1,17 +1,42 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
 
 PROXY = {'proxy_url':'socks5://t1.learn.python.ru:1080',
 'urllib3_proxy_kwargs':{'username':'learn','password':'python'}}
 
+#Настройки лога
+logging.basicConfig(format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+	level = logging.INFO,
+	filename = 'bot.log'
+	)
+
+
+def greet_user(bot,update):
+	text = '😉'
+	print(text)
+	logging.info('text')
+	update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+	#принимаем текст от пользователя
+	user_text = "Привет {}! Ты написал: {}".format(update.message.chat.first_name,update.message.text)
+	print(update.message)
+	#отправляем текст пользователю
+	update.message.reply_text(user_text)
+
 def main():
 	'''Тело бота. Главная функция.'''
-	mybot = Updater("659874606:AAH7vMN9q6Lxrcj2J0LRSRX9tfn3L", 
-		request_kwargs=PROXY)
+	mybot = Updater("659874606:AAH7vMN9q6Lxrcj2J0LRSRX9tfn3LZHT-Jo",request_kwargs=PROXY)
 
+	logging.info('бот запускается')
+	
 	dp = mybot.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
+	dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
 
 	mybot.start_polling()
 	mybot.idle()
+
 
 main()
